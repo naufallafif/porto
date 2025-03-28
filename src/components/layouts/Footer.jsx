57193a94cab92a7164692732161af14ba7de5ef5
peepz <naufalafif2207@gmail.com>
2025-03-28 16:51:50 +0700

    fix

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
-    <div className="font-thin mt-3 ">
+    <div className="font-thin mt-3 sticky ">
         Copyright 2025. All rights reserved. <br />
-        <b>Naufal</b> Afif
+        <p className="font-semibold text-purple-500">Naufal Afif</p>
     </div>
   )
 }
