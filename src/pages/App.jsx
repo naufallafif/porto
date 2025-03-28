db32f671812d768092cabc8b714a69c48756b283
peepz <naufalafif2207@gmail.com>
2025-03-28 16:29:45 +0700

    move folder

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import { InfinitySpin } from "react-loader-spinner";
 import Lenis from "@studio-freight/lenis"; 
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Technologies from "./components/Technologies";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import "../App.css";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Technologies from "../components/Technologies";
+import Experience from "../components/Experience";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
